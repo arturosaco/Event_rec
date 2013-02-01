5bae5cef927ace5d9992f07f7dba082cc257de49
Arturo Sanchez <arturo.sanchezcorrea@gmail.com>
2013-02-01 12:34:57 +0000

    add parelization to cross validation

--- src/3_gbm.R
+++ src/3_gbm.R
@@ -1,10 +1,14 @@
 library(ProjectTemplate)
 load.project()
 
+
+library(doMC)
+registerDoMC(4)
+getDoParWorkers()
+
 # =============
 # = Functions =
 # =============
-
 
 fit.gbm.model <- function(data.big, interaction.par, max.n.trees, 
   shrinkage.par, distribution.par){
@@ -24,21 +28,25 @@ fit.gbm.model <- function(data.big, interaction.par, max.n.trees,
     distribution = distribution.par, 
     n.trees = max.n.trees,
     interaction.depth = interaction.par,
-    shrinkage = shrinkage.par)
+    shrinkage = shrinkage.par, 
+    verbose = FALSE)
 
   fit <- data.frame(data.big[, c("user_id", "event_id")], fit = mod.gbm$fit)
   mod.gbm
 }
 
-test.gbm.model <- function(data.small, model){
+test.gbm.model <- function(data.small, model, range.trees){
   X.new <- model.matrix(formu, data = data.small)
-  preds <- data.frame(data.small[, c("user_id", "event_id", "interested.num")],
-    preds = predict(object = model, newdata = X.new, n.trees = 100))
-  mapk.user <- ddply(preds, "user_id", function(sub){
-      mapk(200, actual = sub[sub$interested.num == 1, "event_id"],
-          predicted = sub[order(sub$preds, decreasing = TRUE), "event_id"])
-    })
-  mean(mapk.user$V1)
+  ldply(round(seq(from = range.trees[1], to = range.trees[2], length = 15)),
+  function(n.trees){
+    preds <- data.frame(data.small[, c("user_id", "event_id", "interested.num")],
+      preds = predict(object = model, newdata = X.new, n.trees = n.trees))
+    mapk.user <- ddply(preds, "user_id", function(sub){
+        mapk(200, actual = sub[sub$interested.num == 1, "event_id"],
+            predicted = sub[order(sub$preds, decreasing = TRUE), "event_id"])
+      })
+    data.frame(n.trees = n.trees, score = mean(mapk.user$V1))
+  })
 }
 
 ### Local TODO
@@ -68,7 +76,6 @@ k <- 5
 partition <- data.frame(user_id = unique(train$user_id),
  partition = sample(1:k, length(unique(train$user_id)), replace = TRUE))
 
-part.int <- 1
 
 part.scores <- ldply(1:k, function(part.int){
   data.big <- train.1[train.1$user_id %in% 
@@ -78,14 +85,15 @@ part.scores <- ldply(1:k, function(part.int){
   mod <- fit.gbm.model(data.big,
      interaction.par = 5,
      shrinkage.par = 0.001,
-     max.n.trees = 100,
+     max.n.trees = 1000,
      distribution = "adaboost")
-  score <- test.gbm.model(data.small, mod)
-  data.frame(partition = part.int, score = score)
-}
+  scores <- test.gbm.model(data.small, mod, range.trees = c(10, 1000))
+  scores$partition <- part.int
+  scores
+}, .progress = "text")
 
-
-
+score.mean <- ddply(part.scores, "n.trees", summarise, score = mean(score))
+ggplot(score.mean, aes(x = n.trees, y = score)) + geom_line()
 
 
 
