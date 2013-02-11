86efa709572740cea8028241c4e39e9848fe90a5
PascalAntoniou <don.pasquale@gmail.com>
2013-02-11 02:24:38 +0000

    factors to numeric tapply

--- src/mean_age_gender.R
+++ src/mean_age_gender.R
@@ -18,14 +18,32 @@ event.attendees.no <- event.attendees.no[event.attendees.no$user_id %in% users$u
 event.attendees.maybe <- event.attendees.maybe[event.attendees.maybe$user_id %in% users$user_id, ]
 event.attendees.invited <- event.attendees.invited[event.attendees.invited$user_id %in% users$user_id, ]
 
+#turn events to factors from numeric (to call tapply later)
 event.attendees.yes$event <- factor(event.attendees.yes$event)
 event.attendees.no$event <- factor(event.attendees.no$event)
 event.attendees.maybe$event <- factor(event.attendees.maybe$event)
 event.attendees.invited$event <- factor(event.attendees.invited$event)
 
+#merge with users, birthyear and gender
 evAtt.yes.uData <- merge(event.attendees.yes,users[,c("user_id", "birthyear", "gender")])
 evAtt.no.uData <- merge(event.attendees.no,users[,c("user_id", "birthyear", "gender")])
 evAtt.maybe.uData <- merge(event.attendees.maybe,users[,c("user_id", "birthyear", "gender")])
 evAtt.invited.uData <- merge(event.attendees.invited,users[,c("user_id", "birthyear", "gender")])
 
- foo <- tapply(evAtt.yes.uData$birthyear,evAtt.yes.uData$event,mean)
+#fix "None" label
+evAtt.yes.uData$birthyear[evAtt.yes.uData$birthyear=="None"] <- NA
+evAtt.no.uData$birthyear[evAtt.no.uData$birthyear=="None"] <- NA
+evAtt.maybe.uData$birthyear[evAtt.maybe.uData$birthyear=="None"] <- NA
+evAtt.invited.uData$birthyear[evAtt.invited.uData$birthyear=="None"] <- NA
+
+#convert factors to numbers quickly
+evAtt.yes.uData$birthyear <- as.numeric(levels(evAtt.yes.uData$birthyear))[evAtt.yes.uData$birthyear]
+evAtt.no.uData$birthyear <- as.numeric(levels(evAtt.no.uData$birthyear))[evAtt.no.uData$birthyear]
+evAtt.maybe.uData$birthyear <- as.numeric(levels(evAtt.maybe.uData$birthyear))[evAtt.maybe.uData$birthyear]
+evAtt.invited.uData$birthyear <- as.numeric(levels(evAtt.invited.uData$birthyear))[evAtt.invited.uData$birthyear]
+
+#tapply to find means by ignoring NA's
+foo <- tapply(evAtt.yes.uData$birthyear,evAtt.yes.uData$event,mean,na.rm=TRUE)
+foo <- tapply(evAtt.no.uData$birthyear,evAtt.no.uData$event,mean,na.rm=TRUE)
+foo <- tapply(evAtt.maybe.uData$birthyear,evAtt.maybe.uData$event,mean,na.rm=TRUE)
+foo <- tapply(evAtt.invited.uData$birthyear,evAtt.invited.uData$event,mean,na.rm=TRUE)
