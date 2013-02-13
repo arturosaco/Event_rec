5cb336b2c4189f2a7b98477becbdb3f8af2cc167
PascalAntoniou <don.pasquale@gmail.com>
2013-02-13 01:58:30 +0000

    added mean lat long

--- src/mean_age_gender.R
+++ src/mean_age_gender.R
@@ -38,10 +38,10 @@ event.attendees.maybe$event <- factor(event.attendees.maybe$event)
 event.attendees.invited$event <- factor(event.attendees.invited$event)
 
 #merge with users, birthyear and gender
-evAtt.yes.uData <- merge(event.attendees.yes,users[,c("user_id", "birthyear", "gender")])
-evAtt.no.uData <- merge(event.attendees.no,users[,c("user_id", "birthyear", "gender")])
-evAtt.maybe.uData <- merge(event.attendees.maybe,users[,c("user_id", "birthyear", "gender")])
-evAtt.invited.uData <- merge(event.attendees.invited,users[,c("user_id", "birthyear", "gender")])
+evAtt.yes.uData <- merge(event.attendees.yes,users[,c("user_id", "birthyear", "gender", "Latitude", "Longitude")])
+evAtt.no.uData <- merge(event.attendees.no,users[,c("user_id", "birthyear", "gender", "Latitude", "Longitude")])
+evAtt.maybe.uData <- merge(event.attendees.maybe,users[,c("user_id", "birthyear", "gender", "Latitude", "Longitude")])
+evAtt.invited.uData <- merge(event.attendees.invited,users[,c("user_id", "birthyear", "gender", "Latitude", "Longitude")])
 
 #fix "None" label
 evAtt.yes.uData$birthyear[evAtt.yes.uData$birthyear=="None"] <- NA
@@ -63,26 +63,34 @@ evAtt.invited.uData <- evAtt.invited.uData[order(evAtt.invited.uData$event),]
 
 #tapply to a)find means by ignoring NA's b)vote for male/female
 evAtt.yes.labels <- data.frame(unique(evAtt.yes.uData$event),
-             tapply(evAtt.yes.uData$birthyear,evAtt.yes.uData$event,mean,na.rm=TRUE),
-             tapply(evAtt.yes.uData$gender,evAtt.yes.uData$event,labelMF))
-colnames(evAtt.yes.labels) <- c("event_id","YesBirthyear","YesGender")
+                               tapply(evAtt.yes.uData$birthyear,evAtt.yes.uData$event,mean,na.rm=TRUE),
+                               tapply(evAtt.yes.uData$gender,evAtt.yes.uData$event,labelMF),
+                               tapply(evAtt.yes.uData$Latitude,evAtt.yes.uData$event,mean,na.rm=TRUE),
+                               tapply(evAtt.yes.uData$Longitude,evAtt.yes.uData$event,mean,na.rm=TRUE))
+colnames(evAtt.yes.labels) <- c("event_id","YesBirthyear","YesGender","YesMeanLat","YesMeanLon")
 
 evAtt.no.labels <- data.frame(unique(evAtt.no.uData$event),
-             tapply(evAtt.no.uData$birthyear,evAtt.no.uData$event,mean,na.rm=TRUE),
-             tapply(evAtt.no.uData$gender,evAtt.no.uData$event,labelMF))
-colnames(evAtt.no.labels) <- c("event_id","NoBirthyear","NoGender")
+                              tapply(evAtt.no.uData$birthyear,evAtt.no.uData$event,mean,na.rm=TRUE),
+                              tapply(evAtt.no.uData$gender,evAtt.no.uData$event,labelMF),
+                              tapply(evAtt.no.uData$Latitude,evAtt.no.uData$event,mean,na.rm=TRUE),
+                              tapply(evAtt.no.uData$Longitude,evAtt.no.uData$event,mean,na.rm=TRUE))
+colnames(evAtt.no.labels) <- c("event_id","NoBirthyear","NoGender","NoMeanLat","NoMeanLon")
 
 evAtt.maybe.labels <- data.frame(unique(evAtt.maybe.uData$event),
-             tapply(evAtt.maybe.uData$birthyear,evAtt.maybe.uData$event,mean,na.rm=TRUE),
-             tapply(evAtt.maybe.uData$gender,evAtt.maybe.uData$event,labelMF))
-colnames(evAtt.maybe.labels) <- c("event_id","MaybeBirthyear","MaybeGender")
+                                 tapply(evAtt.maybe.uData$birthyear,evAtt.maybe.uData$event,mean,na.rm=TRUE),
+                                 tapply(evAtt.maybe.uData$gender,evAtt.maybe.uData$event,labelMF),
+                                 tapply(evAtt.maybe.uData$Latitude,evAtt.maybe.uData$event,mean,na.rm=TRUE),
+                                 tapply(evAtt.maybe.uData$Longitude,evAtt.maybe.uData$event,mean,na.rm=TRUE))
+colnames(evAtt.maybe.labels) <- c("event_id","MaybeBirthyear","MaybeGender","MaybeMeanLat","MaybeMeanLon")
 
 evAtt.invited.labels <- data.frame(unique(evAtt.invited.uData$event),
-             tapply(evAtt.invited.uData$birthyear,evAtt.invited.uData$event,mean,na.rm=TRUE),
-             tapply(evAtt.invited.uData$gender,evAtt.invited.uData$event,labelMF))
-colnames(evAtt.invited.labels) <- c("event_id","InvBirthyear","InvGender")
+                                   tapply(evAtt.invited.uData$birthyear,evAtt.invited.uData$event,mean,na.rm=TRUE),
+                                   tapply(evAtt.invited.uData$gender,evAtt.invited.uData$event,labelMF),
+                                   tapply(evAtt.invited.uData$Latitude,evAtt.invited.uData$event,mean,na.rm=TRUE),
+                                   tapply(evAtt.invited.uData$Longitude,evAtt.invited.uData$event,mean,na.rm=TRUE))
+colnames(evAtt.invited.labels) <- c("event_id","InvBirthyear","InvGender","InvMeanLat","InvMeanLon")
 
-save("evAtt.yes.labels",file="data/evAtt.yes.labels.RData")
-save("evAtt.no.labels",file="data/evAtt.no.labels.RData")
-save("evAtt.maybe.labels",file="data/evAtt.maybe.labels.RData")
-save("evAtt.invited.labels",file="data/evAtt.invited.labels.RData")
+save("evAtt.yes.labels",file="data/evAtt.yes.labels2.RData")
+save("evAtt.no.labels",file="data/evAtt.no.labels2.RData")
+save("evAtt.maybe.labels",file="data/evAtt.maybe.labels2.RData")
+save("evAtt.invited.labels",file="data/evAtt.invited.labels2.RData")
